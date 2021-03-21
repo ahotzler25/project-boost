ebb472df1b4e795ea10da81fd72f79affe67276a
Andy Hotzler <ahotzler@gmail.com>
2021-03-20 23:32:55 -0500

    Refactored code and added debug/hack keys for easy level manipulation

--- Assets/CollisionHandler.cs
+++ Assets/CollisionHandler.cs
@@ -4,15 +4,44 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] float levelLoadDelay = 1f;
+    [SerializeField] AudioClip levelComplete;
+    [SerializeField] AudioClip playerExplosion;
+
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem crashParticles;
+
+    AudioSource audioSource;
+    
+
+    bool isTransitioning = false;
+    bool collisionDisabled = false;
+
+    private void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update() {
+        RespondToHackerKeys();
+    }
+
+    private void RespondToHackerKeys() {
+        if (Input.GetKeyDown(KeyCode.L)) {
+            LoadNextScene();
+        }
+
+        if (Input.GetKeyDown(KeyCode.C)) {
+            collisionDisabled = !collisionDisabled;
+        }
+    }
 
     private void OnCollisionEnter(Collision other) {
+        if (isTransitioning || collisionDisabled) {
+            return;
+        }
+
         switch (other.gameObject.tag) {
-            case "Ground":
-                StartCrashSequence();
-                break;
-            case "Obstacle":
-            // Invoke allows us to delay a method by X seconds (remember to remove the parentheses)
-                StartCrashSequence();
+            case "Friendly":
+                Debug.Log("This is a friendly platform.");
                 break;
             case "Fuel":
                 Debug.Log("You fueled up!");
@@ -21,24 +50,26 @@ public class CollisionHandler : MonoBehaviour
                 StartSuccessSequence();
                 break;
             default:
-                Invoke("ReloadScene", 2f);
+                StartCrashSequence();
                 break;
         }
     }
     private void StartSuccessSequence() {
-        /* TODO add SFX upon success
-        // TODO add particle effect upon success
-        */
+        
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(levelComplete);
+        successParticles.Play();
         GetComponent<Movement>().enabled = false;
         Invoke("LoadNextScene", levelLoadDelay);
     }
 
     private void StartCrashSequence() {
-        /* TODO add SFX upon crash
-        // TODO add particle effect upon crash
-        */
+        isTransitioning = true;
+        audioSource.Stop();
+        crashParticles.Play();
         GetComponent<Movement>().enabled = false;
-        GetComponent<AudioSource>().enabled = false;
+        audioSource.PlayOneShot(playerExplosion);
         Invoke("ReloadScene", levelLoadDelay);
     }
 
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,11 +4,21 @@ using UnityEngine; // namespace where MonoBehavior data lives
 
 public class Movement : MonoBehaviour
 {
+    // PARAMETERS -- For tuning, typically set in the editor;
+    // CACHE -- e.g. refrences for readability or speed;
+    // STATE -- private instance (member) variables;
+
     // Member variable (available throughout entire class)
-    Rigidbody rb;
-    AudioSource audioSource;
     [SerializeField] float thrustForce;
     [SerializeField] float rotationThrust;
+    [SerializeField] AudioClip mainEngine;
+
+    [SerializeField] ParticleSystem mainBoosterParticles;
+    [SerializeField] ParticleSystem leftBoosterParticles;
+    [SerializeField] ParticleSystem rightBoosterParticles;
+
+    Rigidbody rb;
+    AudioSource audioSource;
 
     void Start() {
         // Caches a reference to our component
@@ -23,29 +33,67 @@ public class Movement : MonoBehaviour
 
     void ProcessThrust() {
         // Use KeyCode.name signature since (string name) sig can be tricky to get correct
-        if (Input.GetKey(KeyCode.Space)) { 
-            rb.AddRelativeForce(Vector3.up * thrustForce * Time.deltaTime);
-            
-            if (!audioSource.isPlaying) {
-                audioSource.Play();
-            }
-        } else {
-            audioSource.Stop();
-        } 
-    }
-
-    void ProcessRotation() {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.Space))
         {
-            ApplyRotation(rotationThrust);
+            StartThrusting();
         }
-        else if (Input.GetKey(KeyCode.D)) {
-            ApplyRotation(-rotationThrust);
+        else
+        {
+            StopThrusting();
         }
     }
 
-    // If private/public is not present, default is always private
-    void ApplyRotation(float rotationThisFrame)
+    private void StartThrusting()
+    {
+        rb.AddRelativeForce(Vector3.up * thrustForce * Time.deltaTime);
+        if (!audioSource.isPlaying) {
+            audioSource.PlayOneShot(mainEngine);
+        }
+
+        if (!mainBoosterParticles.isPlaying) {
+            mainBoosterParticles.Play();
+        }
+    }
+    
+    private void StopThrusting() {
+        audioSource.Stop();
+        mainBoosterParticles.Stop();
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A)) {
+            RotateLeft();
+        } else if (Input.GetKey(KeyCode.D)) {
+            RotateRight();
+        } else {
+            StopRotating();
+        }
+    }
+
+    private void RotateLeft()
+    {
+        ApplyRotation(rotationThrust);
+            if (!rightBoosterParticles.isPlaying)
+            {
+                rightBoosterParticles.Play();
+            }
+    }
+
+    private void RotateRight() {
+        ApplyRotation(-rotationThrust);
+        if (!leftBoosterParticles.isPlaying)
+        {
+            leftBoosterParticles.Play();
+        }
+    }
+
+    private void StopRotating() {
+        leftBoosterParticles.Stop();
+        rightBoosterParticles.Stop();
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
     {
         rb.freezeRotation = true; // freezing rotation so we can manually rotate;
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
