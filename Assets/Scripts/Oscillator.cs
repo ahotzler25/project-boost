9353344e67ad104fa2b23874563b80fbe3a793e0
Andy Hotzler <ahotzler@gmail.com>
2021-04-03 21:34:25 -0500

    lost level changes; readded oscillator script to objects, built a couple levels, no skybox or addl assets yet

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -8,6 +8,7 @@ public class Oscillator : MonoBehaviour
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
     [SerializeField] [Range(0,1)] float movementFactor;
+    [SerializeField] float period = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,13 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period; // continually growing over time
+
+        const float tau = Mathf.PI * 2; // tau === 2(PI) (const value of 6.283 <-- which is tau: one full circle)
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f; // recalculated so that rawSinWave now goes from 0 to 1 so it's cleaner
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
